feaefa2310be8965536775cc33462265d68ef8c7
Muammaruddin <muammaruddin8@gmail.com>
2024-05-01 03:22:12 +0800

    first:commit

---
+++ src/scripts/index.js
@@ -0,0 +1,32 @@
+import 'regenerator-runtime';
+import '../styles/main.css';
+import '../styles/responsive.css';
+import './data-display.js';
+
+const navbarDrawerElement = document.querySelector('#navigation-drawer-id');
+const navbarContainerElement = document.querySelector('#navbar-container-id');
+const navbarElement = document.querySelector('#navbar-id')
+const bodyGeneralElement = document.querySelector('body');
+
+navbarDrawerElement.addEventListener('click', event => {
+    navbarContainerElement.classList.toggle('open');
+    event.stopPropagation();
+});
+
+bodyGeneralElement.addEventListener('click', event => {
+    navbarContainerElement.classList.remove('open');
+    event.stopPropagation();
+});
+
+window.addEventListener('scroll', () => {
+    const scrollOnPage = window.pageYOffset;
+  
+    if (scrollOnPage > 0 ) {
+      navbarElement.style.backgroundColor = '#25282d';
+      navbarElement.style.position = 'fixed';
+    } else {
+      navbarElement.style.backgroundColor = 'transparent';
+      navbarElement.style.position = 'absolute';
+    }
+  
+  });
